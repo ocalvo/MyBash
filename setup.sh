32a936693f73fda4ab286585bc6c43b8dfe1a457
Oscar Calvo <oscar@calvonet.com>
2020-08-16 13:26:55 -0700

    Add Setup

--- setup.sh
+++ setup.sh
@@ -1,6 +1,10 @@
 
 sudo apt update
 
+sudo apt install golang-go
+go get -u github.com/justjanne/powerline-go
+cp ./.bashrc ~/.bashrc
+
 ln -s /mnt/c/Users/oscar/_vimrc ~/_vimrc
 ln -s /mnt/c/Users/oscar/_viminfo ~/_viminfo
 ln -s /mnt/c/Users/oscar/Documents ~/Documents
@@ -9,4 +13,20 @@ sudo apt install dos2unix
 dos2unix ~/Documents/WindowsPowerShell/profile.vim
 dos2unix ~/Documents/WindowsPowerShell/vimfiles/autoload/pathogen.vim
 
+
+git config --global user.name "Oscar Calvo"
+git config --global user.email "oscar@calvonet.com"
+git config --global log.date local
+git config --global core.autocrlf false
+
+git config --global diff.tool bc
+git config --global difftool.prompt false
+git config --global difftool.bc trustExitCode true
+
+git config --global merge.tool bc
+git config --global mergetool.prompt false
+git config --global mergetool.bc trustExitCode true
+
+git config --global difftool.bc.path "/mnt/c/Program\ Files/Beyond\ Compare\ 4/BComp.exe"
+git config --global mergetool.bc.path "/mnt/c/Program\ Files/Beyond\ Compare\ 4/BComp.exe"
 
